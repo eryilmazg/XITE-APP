c0ad5dbf890c6c264dec2707c9327a356c352982
eryilmazg <gokhaneryilmazo@gmail.com>
2021-04-25 21:41:13 +0300

    Stories :: card.stories :: init

---
+++ src/stories/1-card.stories.tsx
@@ -0,0 +1,12 @@
+import Card from '../components/songCard';
+
+export const SongCard = () => (
+  <Card 
+    img='https://raw.githubusercontent.com/XiteTV/frontend-coding-exercise/679a82b1e7110c16e14412f1debaa118c10078a9/images/501437/images/app/w522_h292.jpg'
+    singer='Pants Velour'
+    songName='All In'
+  />
+);
+
+const exportInfo = { title: 'Song Card'};
+export default exportInfo;
