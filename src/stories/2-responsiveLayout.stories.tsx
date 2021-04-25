eeb58c19b5af16826b38ef45595716016c8e42a7
eryilmazg <gokhaneryilmazo@gmail.com>
2021-04-25 22:03:50 +0300

    Stories :: responsiveLayout :: init

---
+++ src/stories/2-responsiveLayout.stories.tsx
@@ -0,0 +1,42 @@
+import ResponsiveLayoutComp from '../components/responsiveLayout/index';
+import Card from '../components/songCard/index';
+import '../styles/index.scss';
+
+export const ResponsiveLayout = () => (
+  <ResponsiveLayoutComp>
+    <Card 
+      img='https://raw.githubusercontent.com/XiteTV/frontend-coding-exercise/679a82b1e7110c16e14412f1debaa118c10078a9/images/501437/images/app/w522_h292.jpg'
+      singer='Pants Velour'
+      songName='All In'
+    />
+    <Card 
+      img='https://raw.githubusercontent.com/XiteTV/frontend-coding-exercise/679a82b1e7110c16e14412f1debaa118c10078a9/images/501437/images/app/w522_h292.jpg'
+      singer='Pants Velour'
+      songName='All In'
+    />
+    <Card 
+      img='https://raw.githubusercontent.com/XiteTV/frontend-coding-exercise/679a82b1e7110c16e14412f1debaa118c10078a9/images/501437/images/app/w522_h292.jpg'
+      singer='Pants Velour'
+      songName='All In'
+    />
+    <Card 
+      img='https://raw.githubusercontent.com/XiteTV/frontend-coding-exercise/679a82b1e7110c16e14412f1debaa118c10078a9/images/501437/images/app/w522_h292.jpg'
+      singer='Pants Velour'
+      songName='All In'
+    />
+    <Card 
+      img='https://raw.githubusercontent.com/XiteTV/frontend-coding-exercise/679a82b1e7110c16e14412f1debaa118c10078a9/images/501437/images/app/w522_h292.jpg'
+      singer='Pants Velour'
+      songName='All In'
+    />
+    <Card 
+      img='https://raw.githubusercontent.com/XiteTV/frontend-coding-exercise/679a82b1e7110c16e14412f1debaa118c10078a9/images/501437/images/app/w522_h292.jpg'
+      singer='Pants Velour'
+      songName='All In'
+    />
+  </ResponsiveLayoutComp>
+  
+);
+
+const exportInfo = { title: 'Responsive Layout'};
+export default exportInfo;
