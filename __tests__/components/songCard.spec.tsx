9f83ade21cab057ed6ab447856e25c35988359dc
eryilmazg <gokhaneryilmazo@gmail.com>
2021-04-25 21:41:22 +0300

    Tests :: songCard.spec.tsx :: init

---
+++ __tests__/components/songCard.spec.tsx
@@ -0,0 +1,16 @@
+import { render } from '@testing-library/react'
+import SongCard from '../../src/components/songCard';
+
+it('should render SongCard with img, singer and songName props', () => {
+  const { getByText, getByAltText } = render(
+    <SongCard 
+      img='https://raw.githubusercontent.com/XiteTV/frontend-coding-exercise/679a82b1e7110c16e14412f1debaa118c10078a9/images/501437/images/app/w522_h292.jpg'
+      singer='Pants Velour'
+      songName='All In'
+    />
+  );
+
+  getByText('Pants Velour');
+  getByText('All In');
+  getByAltText('All In')
+});
