12b1ed17b1b29c088fcdecaeb55aefc05eab3dca
eryilmazg <gokhaneryilmazo@gmail.com>
2021-04-26 22:16:07 +0300

    Context :: ApplicationContext :: init

---
+++ src/contexts/index.tsx
@@ -0,0 +1,87 @@
+import React, {
+  createContext, useContext, useState, useEffect, useMemo
+} from 'react';
+import { getSongInfo } from '../api/index';
+import { Video, Genre } from '../types/index';
+import { convertGenresToFilterOptions, convertVideosToYearOptions, filterSongsBySearchGenreYear } from './helpers/index';
+
+const ApplicationContext = createContext({});
+
+export const useApplicationContext = useContext(ApplicationContext);
+
+interface Props {
+  children: React.ReactNode
+}
+
+const ApplicationContextProvider: React.FC<Props> = ({
+  children
+}) => {
+  const [allSongsData, setAllSongsData] = useState<Video[]>([]);
+  const [filteredSongsData, setFilteredSongsData] = useState<Video[]>([]);
+  const [allGenres, setAllGenres] = useState<Genre[]>([]);
+  const [selectedGenres, setSelectedGenres] = useState([]);
+  const [selectedYears, setSelectedYears] = useState([]);
+  const [searchInputValue, setSearchInputValue] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    try {
+      const getSongs = async () => {
+        const response = await getSongInfo();
+        const videos = response.data.videos;
+        const genres = response.data.genres;
+        setAllSongsData(videos);
+        setFilteredSongsData(videos);
+        setAllGenres(genres);
+      };
+
+      getSongs();
+    } catch(e) {
+      if (e.message) {
+        setErrorMessage(e.message);
+        return;
+      }
+      setErrorMessage('Unexpected error occured, please try again.')
+    }
+  }, [])
+
+  useEffect(() => {
+    const filteredSongs = filterSongsBySearchGenreYear(searchInputValue, selectedGenres, selectedYears, allSongsData);
+    setFilteredSongsData(filteredSongs);
+  }, [searchInputValue, selectedGenres, selectedYears]);
+
+  const genresOptions = useMemo(() => {
+    if (allGenres.length !== 0) {
+      return convertGenresToFilterOptions(allGenres);
+    }
+
+    return [];
+  }, [allGenres]);
+
+  const yearOptions = useMemo(() => {
+    if (allSongsData.length !== 0) {
+      return convertVideosToYearOptions(allSongsData);
+    }
+
+    return [];
+  }, [allSongsData]);
+
+  const value = {
+    filteredSongsData,
+    errorMessage,
+    allGenres,
+    genresOptions,
+    yearOptions,
+    setSelectedGenres,
+    setSelectedYears,
+    setSearchInputValue
+  }
+
+  return (
+    <ApplicationContext.Provider value={value}>
+      {children}
+    </ApplicationContext.Provider>
+  )
+};
+
+export default ApplicationContextProvider;
