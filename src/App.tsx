4d239583f413f1ed4204b6e67499f010cb5e4512
eryilmazg <gokhaneryilmazo@gmail.com>
2021-04-26 23:44:41 +0300

    App :: render app components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import ApplicationContextProvider from './contexts/index';
+import SongCards from './containers/songCards/index';
+import Filters from './containers/filters/index';
 import './styles/index.scss';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <ApplicationContextProvider>
+      <Filters />
+      <SongCards />
+    </ApplicationContextProvider>
   );
 }
 
