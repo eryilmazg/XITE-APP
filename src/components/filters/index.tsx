c88cdfda9db7fc52ed0c3f71e84672e501304a67
eryilmazg <gokhaneryilmazo@gmail.com>
2021-04-26 23:44:08 +0300

    Components :: Filters :: init

---
+++ src/components/filters/index.tsx
@@ -0,0 +1,63 @@
+import React, { Dispatch } from 'react';
+import Select from 'react-select';
+import SearchInput from '../../components/searchInput';
+import { Option } from '../../types/index';
+
+interface Props {
+  genresOptions: Option<number>[],
+  yearOptions: Option<number>[],
+  setSelectedGenres: Dispatch<number[]>,
+  setSelectedYears: Dispatch<number[]>,
+  setSearchInputValue: Dispatch<string>,
+  isLoading: boolean
+}
+
+const Filters: React.FC<Props> = ({
+  genresOptions,
+  yearOptions,
+  setSelectedGenres,
+  setSelectedYears,
+  setSearchInputValue,
+  isLoading
+}) => {
+
+  const normalizeSelectedData = (data: Option<number>[]): number[] => (
+    data.map(({ value }) => value)
+  );
+
+  return (
+    !isLoading ? (
+      <div className="filter">
+        <SearchInput
+          onChange={setSearchInputValue}
+        />
+        <div className="filter-select">
+          <div className="filter-select-container">
+            {genresOptions.length > 0 && (
+              <Select
+                classNamePrefix='list'
+                options={genresOptions}
+                isMulti={true}
+                placeholder='Genres'
+                onChange={(data) => setSelectedGenres(normalizeSelectedData(data as Option<number>[]))}
+              />
+            )}
+          </div>
+          <div className="filter-select-container">
+            {yearOptions.length > 0 && (
+              <Select
+                classNamePrefix='list'
+                options={yearOptions}
+                isMulti={true}
+                placeholder='Release year'
+                onChange={(data) => setSelectedYears(normalizeSelectedData(data as Option<number>[]))}
+              />
+            )}
+          </div>
+        </div>
+      </div>
+    ) : null
+  )
+}
+
+export default Filters;
