efc40d1117a4ddf7d972f6309c256543209ee3b0
eryilmazg <gokhaneryilmazo@gmail.com>
2021-04-26 23:42:25 +0300

    Helpers :: convertVideosToYearOptions fix for return object

--- src/contexts/helpers/index.tsx
+++ src/contexts/helpers/index.tsx
@@ -7,26 +7,35 @@ export const convertGenresToFilterOptions = (genres: Genre[]): Option<number>[] 
   }))
 );
 
-export const convertVideosToYearOptions = (videos: Video[]): Option<number>[] => (
-  videos.map(({ release_year }) => ({
+export const convertVideosToYearOptions = (videos: Video[]): Option<number>[] => {
+  let uniqueYears: number[] = [];
+  videos.forEach(({ release_year }) => {
+    if (!uniqueYears.includes(release_year)) {
+      uniqueYears.push(release_year);
+    }
+  })
+  uniqueYears = uniqueYears.sort().reverse();
+  return uniqueYears.map(( release_year ) => ({
     value: release_year,
     label: release_year.toString()
   }))
-);
+}
+  
 
 export const filterSongsBySearchGenreYear = (searchValue: string, genres: number[], years: number[], songsToFilter: Video[]): Video[] => {
   let filteredSongs = songsToFilter;
   if (searchValue) {
     filteredSongs = filteredSongs.filter(({ artist, title }) => (
-      artist.toLowerCase().includes(searchValue.toLowerCase()) || title.toLowerCase().includes(searchValue.toLowerCase())
-    ));
+      artist.toLowerCase().includes(searchValue.toLowerCase()) || title.toString().toLowerCase().includes(searchValue.toLowerCase())
+    )
+    );
   }
 
   if (filteredSongs.length > 0 && (genres.length > 0 || years.length > 0) ) {
     filteredSongs = filteredSongs.filter(({ genre_id, release_year }) => {
       const isGenreMatched = genres.find(g => g === genre_id);
       const isReleaseYearMatched = years.find(y => y === release_year);
-      return !!isGenreMatched || !!isReleaseYearMatched 
+      return (genres.length === 0 || !!isGenreMatched) && (years.length === 0 || !!isReleaseYearMatched )
     })
   }
 
