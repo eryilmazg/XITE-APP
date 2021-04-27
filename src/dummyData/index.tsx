03cfcbc385c6fdb15fb7380261dc28c81a774315
eryilmazg <gokhaneryilmazo@gmail.com>
2021-04-27 23:25:17 +0300

    DummyData :: data for genres and options

--- src/dummyData/index.tsx
+++ src/dummyData/index.tsx
@@ -1,50 +1,101 @@
 export const dummySongs = [
   {
-      "id": 501437,
-      "artist": "Pants Velour",
-      "title": "All In",
-      "release_year": 2014,
-      "genre_id": 14,
-      "image_url": "https://raw.githubusercontent.com/XiteTV/frontend-coding-exercise/679a82b1e7110c16e14412f1debaa118c10078a9/images/501437/images/app/w522_h292.jpg"
-    },
-    {
-      "id": 501649,
-      "artist": "El Koala",
-      "title": "Veni paca to",
-      "release_year": 2014,
-      "genre_id": 8,
-      "image_url": "https://raw.githubusercontent.com/XiteTV/frontend-coding-exercise/679a82b1e7110c16e14412f1debaa118c10078a9/images/501649/images/app/w522_h292.jpg"
-    },
-    {
-      "id": 501895,
-      "artist": "Tom Petty and the Heartbreakers",
-      "title": "I Should Have Known It",
-      "release_year": 2010,
-      "genre_id": 8,
-      "image_url": "https://raw.githubusercontent.com/XiteTV/frontend-coding-exercise/679a82b1e7110c16e14412f1debaa118c10078a9/images/501895/images/app/w522_h292.jpg"
-    },
-    {
-      "id": 502139,
-      "artist": "Waka Flocka Flame",
-      "title": "Grove St. Party",
-      "release_year": 2011,
-      "genre_id": 6,
-      "image_url": "https://raw.githubusercontent.com/XiteTV/frontend-coding-exercise/679a82b1e7110c16e14412f1debaa118c10078a9/images/502139/images/app/w522_h292.jpg"
-    },
-    {
-      "id": 502992,
-      "artist": "Beyoncé",
-      "title": "Single Ladies (Put a Ring on It)",
-      "release_year": 2008,
-      "genre_id": 62,
-      "image_url": "https://raw.githubusercontent.com/XiteTV/frontend-coding-exercise/679a82b1e7110c16e14412f1debaa118c10078a9/images/502992/images/app/w522_h292.jpg"
-    },
-    {
-      "id": 503026,
-      "artist": "John Mayer",
-      "title": "Something Like Olivia",
-      "release_year": 2013,
-      "genre_id": 5,
-      "image_url": "https://raw.githubusercontent.com/XiteTV/frontend-coding-exercise/679a82b1e7110c16e14412f1debaa118c10078a9/images/503026/images/app/w522_h292.jpg"
-    },
+    "id": 501437,
+    "artist": "Pants Velour",
+    "title": "All In",
+    "release_year": 2014,
+    "genre_id": 14,
+    "image_url": "https://raw.githubusercontent.com/XiteTV/frontend-coding-exercise/679a82b1e7110c16e14412f1debaa118c10078a9/images/501437/images/app/w522_h292.jpg"
+  },
+  {
+    "id": 501649,
+    "artist": "El Koala",
+    "title": "Veni paca to",
+    "release_year": 2014,
+    "genre_id": 8,
+    "image_url": "https://raw.githubusercontent.com/XiteTV/frontend-coding-exercise/679a82b1e7110c16e14412f1debaa118c10078a9/images/501649/images/app/w522_h292.jpg"
+  },
+  {
+    "id": 501895,
+    "artist": "Tom Petty and the Heartbreakers",
+    "title": "I Should Have Known It",
+    "release_year": 2010,
+    "genre_id": 8,
+    "image_url": "https://raw.githubusercontent.com/XiteTV/frontend-coding-exercise/679a82b1e7110c16e14412f1debaa118c10078a9/images/501895/images/app/w522_h292.jpg"
+  },
+  {
+    "id": 502139,
+    "artist": "Waka Flocka Flame",
+    "title": "Grove St. Party",
+    "release_year": 2011,
+    "genre_id": 6,
+    "image_url": "https://raw.githubusercontent.com/XiteTV/frontend-coding-exercise/679a82b1e7110c16e14412f1debaa118c10078a9/images/502139/images/app/w522_h292.jpg"
+  },
+  {
+    "id": 503677,
+    "artist": "School is Cool",
+    "title": "Envelop Me",
+    "release_year": 2014,
+    "genre_id": 45,
+    "image_url": "https://raw.githubusercontent.com/XiteTV/frontend-coding-exercise/679a82b1e7110c16e14412f1debaa118c10078a9/images/503677/images/app/w522_h292.jpg"
+  },
+  {
+    "id": 503026,
+    "artist": "John Mayer",
+    "title": "Something Like Olivia",
+    "release_year": 2013,
+    "genre_id": 5,
+    "image_url": "https://raw.githubusercontent.com/XiteTV/frontend-coding-exercise/679a82b1e7110c16e14412f1debaa118c10078a9/images/503026/images/app/w522_h292.jpg"
+  },
+];
+
+export const dummyGenres = [
+  { "id": 5, "name": "Pop" },
+  { "id": 6, "name": "Electronic/Dance" },
+  { "id": 8, "name": "Rock" },
+  { "id": 14, "name": "Rap/Hip-Hop" },
+
+  { "id": 45, "name": "Alternative/Indie" },
+];
+export const dummyGenreOptions = [
+  {
+    value: 5,
+    label: 'Pop'
+  },
+  {
+    value: 6,
+    label: 'Electronic/Dance'
+  },
+  {
+    value: 8,
+    label: 'Rock'
+  },
+  {
+    value: 14,
+    label: 'Rap/Hip-Hop'
+  },
+  {
+    value: 45,
+    label: 'Alternative/Indie'
+  },
+  
+];
+
+export const dummyYearOptions = [
+  {
+    value: 2014,
+    label: '2014'
+  },
+  {
+    value: 2013,
+    label: '2013'
+  },
+  {
+    value: 2011,
+    label: '2011'
+  },
+  {
+    value: 2010,
+    label: '2010'
+  },
 ];
